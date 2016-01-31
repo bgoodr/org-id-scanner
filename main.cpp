42acbea12b0367a7ebf59e26a2ba076c89123d59
Brent Goodrick <bgoodr@gmail.com>
2016-01-31 12:15:46 -0800

    WIP checkin (does not compile yet) Stub out the class and add some test .org files to feed to it.

--- main.cpp
+++ main.cpp
@@ -21,14 +21,81 @@
 // #include <unistd.h>
 // #include <vector>
 
+#include <sys/types.h> // opendir
+#include <dirent.h>    // opendir
+
+
 #ifndef assert
 #  define assert(x) if (!(x)) { std::cout << __FILE__ << ":" << __LINE__ << ":" << "ASSERTION FAILED: " #x " is not true" << std::endl; }
 #endif
 
+class OrgIdScanner {
+public:
+  OrgIdScanner() {}
+  
+  bool parseDirectory(const std::string & inDir)
+  {
+    DIR * dirfp = opendir(inDir.c_str());
+
+    if (!dirfp) {
+      std::cerr << "ERROR: Failed to open directory: " << inDir << std::endl;
+      return false;
+    }
+
+    struct dirent entry;
+    struct dirent *result = NULL;
+    int readdir_retval = 0;
+    while ((readdir_retval = readdir_r(dirfp, &entry, &result)) == 0 && result) {
+      std::cout << __FILE__ << ":" << __LINE__ << ":" << "entry.d_name " << entry.d_name << std::endl;
+    }
+
+    if (readdir_retval) {
+      std::cerr << "ERROR: Failed to read from directory: " << inDir << std::endl;
+      return false;
+    }
+
+    closedir(dirfp);
+    return true;
+  }
+  
+}
+
+
+
 int main(int argc, char *argv[], char *const envp[])
 {
   std::cout << __FILE__ << ":" << __LINE__ << ":" << "main begin" << std::endl;
-  assert(false);
+
+  std::string inDir;
+  for (int i = 0; i < argc; i++)
+  {
+    std::string arg = argv[i];
+    if (arg.empty()) break;
+    if (arg == "-d") {
+      if (i + 1 >= argc) {
+        std::cerr << "ERROR: Missing value of -d option." << std::endl;
+        return 1;
+      }
+      inDir = argv[++i];
+    }
+  }
+
+  if (inDir.empty()) {
+    std::cerr << "ERROR: -d option was not specified." << std::endl;
+    return 1;
+  }
+
+  //std::cout << __FILE__ << ":" << __LINE__ << ":" << "inDir " << inDir << std::endl;
+
+  OrgIdScanner scanner;
+  int success = scanner.parseDirectory(inDir);
+  if (!success) {
+    return 1;
+  }
+
+  // Write out the org id file:
+  
+
   std::cout << __FILE__ << ":" << __LINE__ << ":" << "main end" << std::endl;
   return 0;
 } // end main
