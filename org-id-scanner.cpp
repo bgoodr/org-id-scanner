99290ca1a04a64d41ad33fe0e069091a40eebb10
Brent Goodrick <bgoodr@gmail.com>
2016-01-31 21:28:47 -0800

    Finish writer

--- org-id-scanner.cpp
+++ org-id-scanner.cpp
@@ -10,14 +10,15 @@
 #include <sys/stat.h>  // stat
 #include <unistd.h>    // stat
 
-#include <set>
-#include <vector>
-
 #include <limits.h>    // realpath
 #include <stdlib.h>    // realpath
 
 #include <string.h>    // strerror_r
 #include <errno.h>     // errno
+
+#include <set>
+#include <vector>
+#include <map>
 
 
 #include <limits.h> // PATH_MAX http://stackoverflow.com/a/9449307/257924
@@ -268,21 +269,68 @@ public:
         return false;
       }
 
+      std::string id = buffer.substr(idValueBegPos, idValueEndPos - idValueBegPos);
+
       if (_verbosity.isAtVerbosity(VerboseNS::E_DEBUG)) {
-        std::cout << "absPath " << absPath << " id " << buffer.substr(idValueBegPos, idValueEndPos - idValueBegPos) << std::endl;
+        std::cout << "absPath " << absPath << " id " << id << std::endl;
       }
+      
+      IdSetT & ids = _map[absPath]; // Create a IdSetT if one does not exist
+      if (ids.find(id) != ids.end()) {
+        std::cerr << "ERROR: Duplicate ID value " << id << " at position " << idValueBegPos << " in file " << absPath << std::endl;
+        return false;
+      }
+      ids.insert(id);
 
     }
       
+    return true;
+  }
+
+  bool writeIdAlistFile(const std::string & alistPath)
+  {
+    std::ofstream stream(alistPath.c_str());
+    if (!stream) {
+      std::cerr << "ERROR: Cannot open " << alistPath << " for writing." << std::endl;
+      return false;
+    }
+
+    bool first = true;
+    stream << "(";
+    for (FileToIdSetT::iterator cur = _map.begin(); cur != _map.end(); ++cur)
+    {
+      const std::string & absPath = cur->first;
+
+      if (!first) {
+        stream << std::endl << " ";
+      }
+
+      stream << "(\"" << absPath << "\" ";
+      IdSetT & ids = cur->second;
+
+      for (IdSetT::iterator cur = ids.begin(); cur != ids.end(); ++cur)
+      {
+        stream << " \"" << *cur << "\"";
+      }
+
+      stream << ")";
+
+      first = false;
+    }
+    stream << ")" << std::endl;
 
     return true;
   }
+
 
 
   DEFINE_SET_VERBOSITY_METHOD(OrgIDParser);
 
 private:
   VerboseNS::VerbosityTracker _verbosity;
+  typedef std::set<std::string> IdSetT;
+  typedef std::map<std::string, IdSetT > FileToIdSetT;
+  FileToIdSetT _map;
 };
 
 int main(int argc, char *argv[], char *const envp[])
@@ -291,7 +339,8 @@ int main(int argc, char *argv[], char *const envp[])
   bool readingDirectories = false;
   typedef std::vector<std::string> DirectoryListT;
   DirectoryListT directories;
-  for (int i = 0; i < argc; i++)
+  std::string idAlistPath;
+  for (int i = 1; i < argc; i++)
   {
     std::string arg = argv[i];
     if (arg.empty()) break;
@@ -304,12 +353,26 @@ int main(int argc, char *argv[], char *const envp[])
         verbosity |= VerboseNS::E_VERBOSE;
       } else if (arg == "-debug") {
         verbosity |= VerboseNS::E_DEBUG;
+      } else if (arg == "-o") {
+        if (i + 1 >= argc) {
+          std::cerr << "ERROR: -o requires a value." << std::endl;
+          return 1;
+        }
+        idAlistPath = argv[++i];
+      } else {
+        std::cerr << "ERROR: Unrecognized option " << arg << std::endl;
+        return 1;
       }
     }
   }
 
   if (directories.empty()) {
     std::cerr << "ERROR: No directories specified." << std::endl;
+    return 1;
+  }
+
+  if (idAlistPath.empty()) {
+    std::cerr << "ERROR: No alistfile specified." << std::endl;
     return 1;
   }
 
@@ -330,7 +393,9 @@ int main(int argc, char *argv[], char *const envp[])
       return 1;
     }
   }
-  // Write out the org id file:
+
+  // Write out the alist file:
+  parser.writeIdAlistFile(idAlistPath);
   
   return 0;
 } // end main
